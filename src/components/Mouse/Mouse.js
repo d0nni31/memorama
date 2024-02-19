8c26e9a422c9ff9fde91fa75dcbae1a673c40602
Donny <donnyvera6@gmail.com>
2024-02-18 19:55:14 -0500

    xdd

---
+++ src/components/Mouse/Mouse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MemoBlock.css';
+
+class DetectarBloque extends React.Component {
+  render() {
+    return (
+      <div className="MemoBlock"> {/* Cambia el nombre de la clase aquí */}
+      </div>
+    );
+  }
+}
+
+export default DetectarBloque;
